8c8467688d4ca9c83e25ed38e4d0baa3e30cb14b
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2020-07-21 23:11:09 +0100

    start creating planning (onclick issue)

--- routes/client/planning.js
+++ routes/client/planning.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { Clients,Users,Groups ,Entities,SubEntities,Plannings,Positions} = require('../../sequelize');
+const { response } = require('express');
 
 
 
@@ -10,5 +11,14 @@ router.get('/create',async function(req, res, next) {
   const client_id = await Clients.findOne( { where:{email :req.user.username} } );
   const groups = await  Groups.findAll( { where:{clientId :client_id.id} } );
   res.render('creation',{groups :groups });
+});
+
+router.get('/create_planning',async function(req, res, next) {
+  var date=new Date();
+  const client_id = await Clients.findOne( { where:{email :req.user.username} } );
+  await Plannings.create({planning_name: req.body.planning_name, clientId: req.body.clientId,start:"",end:"",
+  date:date.getFullYear+"-"+date.getMonth+"-"+date.getDate}).then((planning)=> {
+    res.redirect("/planning_control/"+planning.id)
+  })
 });
 module.exports = router;
